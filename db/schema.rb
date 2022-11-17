565afcf1c78143f0f7f3efc301afbbc352d7010e
hwlei888 <leihuawen888@gmail.com>
2022-11-18 05:09:38 +1100

    postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2022_11_12_133321) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "cars", force: :cascade do |t|
     t.string "registration"
     t.string "name"
